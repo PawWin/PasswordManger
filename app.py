02c0b60f8e71ab8c1fa827bde1f75586aded38e2
Balon02 <147401028+Balon02@users.noreply.github.com>
2024-01-11 22:34:40 +0100

    cleaned unnecessary arguments from app file

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from flask_login import login_user, current_user, logout_user, login_required
 from sqlalchemy.exc import IntegrityError
 import random
 import string
+
+
 @app.route('/')
 def base():
     if forms.RegistrationForm().validate_on_submit():
-        # Creating a new user in the database
         register_form = forms.RegistrationForm()
         hashed_password = bcrypt.generate_password_hash(register_form.password.data).decode('utf-8')
         user = User(username=register_form.username.data,
@@ -20,18 +21,15 @@ def base():
             db.session.commit()
         except IntegrityError:
             return redirect(url_for('base'))
-        # Signing in the user after creating them
         user = User.query.filter_by(email=forms.RegistrationForm().email.data).first()
         if user and bcrypt.check_password_hash(user.password, forms.RegistrationForm().password.data):
             login_user(user)
-            # Taking the user to the authenticated side of the site
             return redirect(url_for('base'))
 
     if forms.LoginForm().validate_on_submit():
         user = User.query.filter_by(email=forms.LoginForm().email.data).first()
         if user and bcrypt.check_password_hash(user.password, forms.LoginForm().password.data):
-            login_user(user, remember=forms.LoginForm().remember.data)
-            #print(current_user.get_all_image_links())
+            login_user(user)
             return redirect(url_for('base'))
 
     if (request.method == "POST") & (request.form.get('post_header') == 'log out'):
