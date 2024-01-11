724948170216aa0005ee145077812378b11c6d4c
Paweł Winkler <147107563+Wajchaa@users.noreply.github.com>
2024-01-11 20:32:22 +0100

    initial html and css

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from config import app
+from flask import render_template
+@app.route('/')
+def base():
+    return render_template('base.html')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app.run(debug=True)
